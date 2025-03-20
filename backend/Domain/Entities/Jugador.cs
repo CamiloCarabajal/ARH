ae2777c534ebd86c99520833c0e2621570f15e3e
CamiloCarabajal <ultrakin3t@gmail.com>
2025-03-20 17:50:48 -0300

    Arquitectura sin referencias

---
+++ backend/Domain/Entities/Jugador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Jugador : User
+    {
+        public int Edad { get; set; }
+        public string Posicion { get; set; }
+        public int Goles { get; set; }
+
+        public List<Partido> PartidosList { get; set; }
+    }
+}
---
+++ backend/Domain/Entities/Partido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Partido
+    {
+        public int Id { get; set; }
+        public int ResultadoPropio { get; set; }
+        public string ResultadoRival { get; set; }
+        public string Rival { get; set; }
+
+        // Relacion con jugadores
+        public List<Jugador> JugadorList { get; set; }
+    }
+}
