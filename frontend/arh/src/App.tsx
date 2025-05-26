76f42a20ba98aea4555d0e4157b2cdb8ce3855a2
CamiloCarabajal <ultrakin3t@gmail.com>
2025-05-26 20:43:22 -0300

    Init frontend

---
+++ frontend/arh/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './components/header/Header'
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Header/>
+    </>
+  )
+}
+
+export default App
